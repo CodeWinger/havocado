8fa7d91b220a29e3f0a37cf220490f2bbf2e72bb
martin.ashton <martin.ashton@e5697e63-5562-e9f5-614e-de4a66b046a8>
2010-10-04 00:30:39 +0000

    modified some stuff to have them return things.

--- trunk/servercode/Commands/RMICommands/DeleteFlightRMICommand.java
+++ trunk/servercode/Commands/RMICommands/DeleteFlightRMICommand.java
@@ -7,16 +7,20 @@ public class DeleteFlightRMICommand extends AbstractRMICommand {
   int id;
   int flightNum;
 
+  boolean success;
+
   public DeleteFlightRMICommand(ResourceManager pRm, int pId, int pFlightNum) {
     super(pRm);
     // Store our attributes.
     id = pId;
     flightNum = pFlightNum;
+    
+    success = false;
   }
   
   public void doCommand() throws Exception {
     // Perform the command.
-    rm.deleteFlight(id, flightNum);
+    success = rm.deleteFlight(id, flightNum);
   }
 }
 
