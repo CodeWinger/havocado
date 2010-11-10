bc49c9f358b8e9084dadfaa73a43d7e36e328dcb
martin.ashton <martin.ashton@e5697e63-5562-e9f5-614e-de4a66b046a8>
2010-11-10 19:33:11 +0000

    Added a default constructor to the exceptions.

--- trunk/servercode/exceptions/TransactionAbortedException.java
+++ trunk/servercode/exceptions/TransactionAbortedException.java
@@ -3,6 +3,10 @@ package exceptions;
 public class TransactionAbortedException extends Exception{
 	public ResInterface.Timestamp t;
 	
+	public TransactionAbortedException() {
+		t = new ResInterface.Timestamp();
+	}
+	
 	public TransactionAbortedException(ResInterface.Timestamp pT){
 		t = pT;
 	}
