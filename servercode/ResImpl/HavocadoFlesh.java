dff2ac8ef613aff92c4cde100bdbdf271dedd436
martin.ashton <martin.ashton@e5697e63-5562-e9f5-614e-de4a66b046a8>
2010-11-11 00:27:25 +0000

    testing

--- servercode/ResImpl/HavocadoFlesh.java
+++ servercode/ResImpl/HavocadoFlesh.java
@@ -849,9 +849,9 @@ public class HavocadoFlesh
 
 	public ReturnTuple<Boolean> commit(int id, Timestamp timestamp) throws RemoteException, TransactionAbortedException, InvalidTransactionException {
 		timestamp.stamp();
-		overseer.commit(id);
+		boolean result = overseer.commit(id);
 		timestamp.stamp();
-		return new ReturnTuple<Boolean>(true, timestamp);
+		return new ReturnTuple<Boolean>(result, timestamp);
 	}
 
 
