ee4273ae0b7f71d61d6dd68ac998ae6027dc14aa
martin.ashton <martin.ashton@e5697e63-5562-e9f5-614e-de4a66b046a8>
2010-11-11 22:39:08 +0000

    sdf

---
+++ trunk/clientsrc/ComputeAverage.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python
+
+
+outputFilename = "PerformanceAnalysis.txt"
+outputFilename2 = "PerformanceAnalysis2.txt"
+filenames = ["benchmark1.log"]
+
+machineCounter = 1
+machineMap = {}
+machineMap2 = {}
+timestamps = {}
+
+def newMachine(name):
+	global machineCounter
+	if not (name in machineMap):
+		machineMap[name] = machineCounter
+		machineCounter = machineCounter + 1
+
+def newMachine2(name):
+	if not (name in machineMap2):
+		machineMap2[name] = []
+
+def processBuffer(buff):
+	size = len(buff)
+	if(size <= 1):
+		return
+	
+	mimiName = buff[0].split(' ')[0]
+	mimiFirstTime = long(buff[0].split(' ')[1])
+	mimiLastTime = long(buff[size-1].split(' ')[1])
+	newMachine2(mimiName)
+	mimiTime = machineMap2[mimiName]
+	mimiTime.append(mimiLastTime - mimiFirstTime);
+	
+	willyName = buff[1].split(' ')[0]
+	willyFirstTime = long(buff[1].split(' ')[1])
+	willyLastTime = long(buff[size-2].split(' ')[1])
+	newMachine2(willyName)
+	willyTime = machineMap2[willyName]
+	willyTime.append(willyLastTime - willyFirstTime);
+	
+	foundLab = 0
+	for i in range(size):
+		line = buff[i]
+		tokens = buff[i].split(' ')
+		if "lab" in tokens[0]:
+			if foundLab == 0:
+				labName = tokens[0]
+				labFirstTime = long(tokens[1])
+				labLastTime = long(buff[i+1].split(' ')[1])
+				newMachine2(labName)
+				labTime = machineMap2[labName]
+				labTime.append(labLastTime - labFirstTime)
+				foundLab = 1
+			else:
+				foundLab = 0
+
+def processFile2(fname):
+	f = open(fname, "r")
+	
+	buff = []
+	for line in f:
+		line = str.rstrip(line)
+		if len(line) > 1:
+			buff.append(line)
+		else:
+			processBuffer(buff)
+			buff = []
+	
+	f.close()
+
+def processFile(fname):
+	f = open(fname, "r")
+
+	for line in f:
+		line = str.rstrip(line)
+		tokens = line.split(' ')
+		if len(line) > 1:
+			machine = tokens[0]
+			newMachine(machine)
+			time = str(tokens[1])
+			timestamps[time] = machineMap[machine]
+		
+	f.close()
+
+def writeOutput(fname):
+	f = open(fname, 'w')
+	for key in machineMap:
+		f.write("#" + key + " : " + str(machineMap[key]));
+		f.write("\n");
+	f.write("#time machine\n")
+	
+	sortedTime = timestamps.keys();
+	sortedTime.sort();
+	
+	for time in sortedTime:
+		f.write(time + " " + str(timestamps[time]) + "\n")
+	
+	f.close()
+
+def writeOutput2(fname):
+
+	f = open(fname, 'w')
+	for key in machineMap2:
+		print key
+		timestampList = machineMap2[key]
+		tSize = len(timestampList)
+		acc = 0
+		for i in range(tSize):
+			print timestampList[i]
+			acc = acc + timestampList[i]
+		avg = float(acc) / float(tSize)
+		f.write("average time spent in " + key + ": " + str(avg) + "ms\n");
+	
+	f.close()
+
+def main():
+	for filename in filenames:
+		processFile(filename)
+		processFile2(filename)
+	
+	writeOutput(outputFilename)
+	writeOutput2(outputFilename2)
+main()
