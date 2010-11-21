a5a9c4d7d3c8ce3953fefac311cafc99aaad1cfe
martin.ashton <martin.ashton@e5697e63-5562-e9f5-614e-de4a66b046a8>
2010-11-21 20:21:44 +0000

    sdf

---
+++ trunk/servercode/ResInterface/MemberInfo.java
@@ -0,0 +1,8 @@
+public class MemberInfo {
+	public String machineName;
+	public String rmiName;
+	public MemberInfo(String pMachineName, String pRmiName) {
+		machineName = pMachineName;
+		rmiName = pRmiName;
+	}
+}
