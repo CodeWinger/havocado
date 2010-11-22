ccd6e9cdcde446f4da7e9acbef399b34892daab8
oogun2000@hotmail.com <oogun2000@hotmail.com@e5697e63-5562-e9f5-614e-de4a66b046a8>
2010-11-22 18:53:37 +0000

    Removed hostname field in MemberInfo

--- trunk/servercode/ResInterface/MemberInfo.java
+++ trunk/servercode/ResInterface/MemberInfo.java
@@ -4,11 +4,9 @@ import java.io.Serializable;
 import java.net.InetAddress;
 
 public class MemberInfo implements Serializable{
-	public String machineName;
 	public String rmiName;
 	public InetAddress address;
-	public MemberInfo(String pMachineName, String pRmiName, InetAddress pAddress) {
-		machineName = pMachineName;
+	public MemberInfo(String pRmiName, InetAddress pAddress) {
 		rmiName = pRmiName;
 		address = pAddress;
 	}
