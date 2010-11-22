8fed9aa97f518250101b0d1262ac2e567004dd33
oogun2000@hotmail.com <oogun2000@hotmail.com@e5697e63-5562-e9f5-614e-de4a66b046a8>
2010-11-22 19:45:52 +0000

    Added abstract method to GroupMember.java

--- trunk/servercode/ResImpl/GroupMember.java
+++ trunk/servercode/ResImpl/GroupMember.java
@@ -1,7 +1,8 @@
 package ResImpl;
 
+import java.util.Collections;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Vector;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -10,6 +11,8 @@ import org.jgroups.Receiver;
 import org.jgroups.View;
 import org.jgroups.protocols.pbcast.NAKACK;
 import org.jgroups.stack.IpAddress;
+import org.jgroups.ChannelClosedException;
+import org.jgroups.ChannelNotConnectedException;
 import org.jgroups.JChannel;
 import org.jgroups.Message;
 import org.jgroups.Address;
@@ -20,8 +23,9 @@ import ResInterface.ResourceManager;
 
 public abstract class GroupMember implements Receiver {
 	private JChannel channel;
-	protected List<MemberInfo> currentMembers = new LinkedList<MemberInfo>();
+	protected LinkedList<MemberInfo> currentMembers = new LinkedList<MemberInfo>();
 	private MemberInfo myInfo = null;
+	protected MemberInfo master = null;
 	protected boolean isMaster;
 	
 	public GroupMember(boolean isMaster, String myRMIServiceName, String groupName) {
@@ -40,10 +44,23 @@ public abstract class GroupMember implements Receiver {
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
+		//TODO Master specific stuff.
+		//TODO Slave specific stuff.
 	}
 	
 	public void promoteToMaster() {
 		// TODO fill this in.
+		master = myInfo;
+		isMaster = true;
+		try {
+			channel.send(null, null, currentMembers);
+		} catch (ChannelNotConnectedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ChannelClosedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public ResourceManager getMaster() {
@@ -61,6 +78,8 @@ public abstract class GroupMember implements Receiver {
 		
 	}
 	
+	protected abstract void specialReceive(Message arg0);
+	
 	public void setState(byte[] arg0) {
 		// TODO Auto-generated method stub
 		
@@ -77,17 +96,40 @@ public abstract class GroupMember implements Receiver {
 	}
 	
 	public void viewAccepted(View arg0) {
-		// TODO Auto-generated method stub
 		Vector<Address> addresses = arg0.getMembers();
-		if (isMaster) {
-			// For everyone in currentmembers, if they aren't in the view, remove from list and send new list.
+		PriorityQueue<Integer> toRemove = new PriorityQueue<Integer>(1, Collections.reverseOrder());
+		boolean found;
+		int position = 0;
+		
+		// For everyone in currentmembers, if they aren't in the view, mark them to be removed.
+		for (MemberInfo mi : currentMembers) {
+			found = false;
 			for (Address a : addresses) {
-				//a.
+				IpAddress ipa = (IpAddress)a;
+				if (ipa.getIpAddress().equals(mi.address)) {
+					found = true;
+					break;
+				}
 			}
+			if (!found)
+				toRemove.add(position);	
+			position++;
 		}
-		else {
-			// If the master dies and we're next in line, become a master.
-			if (addresses.contains(currentMembers.get(0)))
+		// Remove all marked members.
+		while (!toRemove.isEmpty())
+			currentMembers.remove(toRemove.poll());
+
+		// If the master dies and we're next in line, become a master.
+		if (!isMaster) {
+			found = false;
+			for (Address a : addresses) {
+				IpAddress ipa = (IpAddress)a;
+				if (ipa.getIpAddress().equals(master.address)) {
+					found = true;
+					break;
+				}
+			}
+			if (!found)
 				promoteToMaster();
 		}
 	}
