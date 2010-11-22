7c0ace7ba195fdaedb500042ae477939dc66b392
oogun2000 <oogun2000@hotmail.com>
2010-11-22 18:12:35 +0000

    Added address field to MemberInfo

--- servercode/ResInterface/MemberInfo.java
+++ servercode/ResInterface/MemberInfo.java
@@ -2,11 +2,15 @@ package ResInterface;
 
 import java.io.Serializable;
 
+import org.jgroups.stack.IpAddress;
+
 public class MemberInfo implements Serializable{
 	public String machineName;
 	public String rmiName;
-	public MemberInfo(String pMachineName, String pRmiName) {
+	public IpAddress address;
+	public MemberInfo(String pMachineName, String pRmiName, IpAddress pAddress) {
 		machineName = pMachineName;
 		rmiName = pRmiName;
+		address = pAddress;
 	}
 }
